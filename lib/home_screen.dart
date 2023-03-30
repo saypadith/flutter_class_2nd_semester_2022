6c55b4408bc5bd7c57c8854d68ae1390ea508cb3
Savath Saypadith <mr.saypadith@gmail.com>
2023-03-30 14:37:04 +0700

    add container

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Screen"),
+        title: Text("ໜ້າຫຼັກ"),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -16,92 +16,67 @@ class HomeScreen extends StatelessWidget {
             children: [
               Container(
                 width: 150,
-                height: 100,
+                height: 150,
                 color: Colors.amber,
-                child: const Center(
-                  child: Text("Box A"),
+                child: Center(
+                  child: const Text("Box A"),
                 ),
               ),
               Container(
                 width: 150,
-                height: 100,
+                height: 150,
                 color: Colors.green,
-                child: const Center(
-                  child: Text("Box B"),
+                child: Center(
+                  child: const Text("Box B"),
                 ),
               ),
             ],
           ),
           Container(
-            margin: EdgeInsets.all(48),
             width: 300,
-            height: 100,
+            height: 150,
+            margin: EdgeInsets.all(24),
             decoration: BoxDecoration(
-              color: Colors.amber,
+              color: Colors.blue,
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(48),
                 bottomRight: Radius.circular(48),
               ),
             ),
-            child: const Center(
-              child: Text("Box C"),
+            child: Center(
+              child: const Text("Box C"),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
                 width: 100,
                 height: 50,
-                color: Colors.blue,
-                child: const Center(
-                  child: Text("Box D"),
+                color: Colors.purple,
+                child: Center(
+                  child: const Text("Box D"),
                 ),
               ),
               Container(
                 width: 100,
                 height: 50,
-                color: Colors.purple,
-                child: const Center(
-                  child: Text("Box E"),
+                color: Colors.pink,
+                child: Center(
+                  child: const Text("Box E"),
                 ),
               ),
               Container(
                 width: 100,
                 height: 50,
                 color: Colors.blueGrey,
-                child: const Center(
-                  child: Text("Box F"),
+                child: Center(
+                  child: const Text("Box F"),
                 ),
               ),
             ],
           ),
-          SizedBox(
-            height: 24,
-          ),
-          Center(child: Text("Sign in with:")),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 48),
-            // color: Colors.indigo,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(
-                  Icons.facebook,
-                  size: 48,
-                ),
-                Icon(
-                  Icons.person,
-                  size: 48,
-                ),
-                Icon(
-                  Icons.settings,
-                  size: 48,
-                ),
-              ],
-            ),
-          )
         ],
       ),
     );
