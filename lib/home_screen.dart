1b7fc81592855b63b05f632185e836c7a1adc7e6
Savath Saypadith <mr.saypadith@gmail.com>
2023-03-30 12:32:08 +0700

    add container

---
+++ lib/home_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 150,
+                height: 100,
+                color: Colors.amber,
+                child: const Center(
+                  child: Text("Box A"),
+                ),
+              ),
+              Container(
+                width: 150,
+                height: 100,
+                color: Colors.green,
+                child: const Center(
+                  child: Text("Box B"),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.all(48),
+            width: 300,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(48),
+                bottomRight: Radius.circular(48),
+              ),
+            ),
+            child: const Center(
+              child: Text("Box C"),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 100,
+                height: 50,
+                color: Colors.blue,
+                child: const Center(
+                  child: Text("Box D"),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 50,
+                color: Colors.purple,
+                child: const Center(
+                  child: Text("Box E"),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 50,
+                color: Colors.blueGrey,
+                child: const Center(
+                  child: Text("Box F"),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Center(child: Text("Sign in with:")),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 48),
+            // color: Colors.indigo,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.facebook,
+                  size: 48,
+                ),
+                Icon(
+                  Icons.person,
+                  size: 48,
+                ),
+                Icon(
+                  Icons.settings,
+                  size: 48,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
