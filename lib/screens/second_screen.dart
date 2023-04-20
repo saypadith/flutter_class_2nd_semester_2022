f38fd4c65f645a8958a852e7a8cc64cf521d8c69
Savath Saypadith <mr.saypadith@gmail.com>
2023-04-20 14:02:13 +0700

    add stack widget

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -8,8 +8,31 @@ class SecondScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Stack")),
       body: Container(
-        child: Center(
-          child: Text("ໜ້າທີ 2"),
+        margin: EdgeInsets.all(16),
+        child: Stack(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: Image.network("https://picsum.photos/id/124/400/250")),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: Colors.black26,
+                ),
+                child: const Text(
+                  "10,000 KIP",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
